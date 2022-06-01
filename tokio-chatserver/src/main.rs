0afacc01ce5badb2a4619d11908989f58475ce7d
dolores <ivo.ruzicka@tutanota.com>
2022-06-01 17:43:18 +0200

    multi client capability

--- tokio-chatserver/src/main.rs
+++ tokio-chatserver/src/main.rs
@@ -1,5 +1,5 @@
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
 };
 
@@ -7,12 +7,22 @@ use tokio::{
 
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-
-    let (mut socket, _addr) = listener.accept().await.unwrap();
     loop {
-        let mut buffer = [0u8; 1024];
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
 
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+            let mut reader = BufReader::new(reader);
+
+            let mut line = String::new();
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
